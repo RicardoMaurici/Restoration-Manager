05aff7ecfeabbefd855cf1d0586a8f986dd1dda1
ricardo maurici <ricardo.maurici@hotmail.com>
2015-03-06 16:58:54 -0300

    add sql

---
+++ sql/rm.sql
@@ -0,0 +1,81 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.10deb1
+-- http://www.phpmyadmin.net
+--
+-- Máquina: localhost
+-- Data de Criação: 06-Mar-2015 às 16:57
+-- Versão do servidor: 5.5.41-0ubuntu0.14.04.1
+-- versão do PHP: 5.5.9-1ubuntu4.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de Dados: `rm`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `car`
+--
+
+CREATE TABLE IF NOT EXISTS `car` (
+  `plate` varchar(7) NOT NULL,
+  `model` varchar(50) NOT NULL,
+  `brand` varchar(50) NOT NULL,
+  `year` int(4) NOT NULL,
+  `color` varchar(25) NOT NULL,
+  PRIMARY KEY (`plate`),
+  UNIQUE KEY `plate` (`plate`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `part`
+--
+
+CREATE TABLE IF NOT EXISTS `part` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `date_shop` date DEFAULT NULL,
+  `price` double DEFAULT NULL,
+  `description` varchar(50) NOT NULL,
+  `inhands` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `section`
+--
+
+CREATE TABLE IF NOT EXISTS `section` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `plate` varchar(7) NOT NULL,
+  `description` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `section_part`
+--
+
+CREATE TABLE IF NOT EXISTS `section_part` (
+  `id_section` int(11) NOT NULL,
+  `id_part` int(11) NOT NULL,
+  PRIMARY KEY (`id_section`,`id_part`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
